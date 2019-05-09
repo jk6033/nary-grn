18d111f5b479d71f07ac4e8a4e26b3dd8a6e95ba
Joohyung Kim <crybaby26@yonsei.ac.kr>
2019-05-09 20:22:24 +0900

    debugging for nan loss issue

--- debug_test/tf_gathernd_test.py
+++ debug_test/tf_gathernd_test.py
@@ -1,24 +1,12 @@
 import tensorflow as tf
+import tensorflow as tf
 
-batch_size = 8
-dag_hidden_dim = 100
-idx_var=tf.constant(0) #tf.Variable(0,trainable=False)
+with tf.device('/cpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b) + tf.constant(1e-10)
+    c = tf.cast(c, dtype=tf.float64)
+    c = c + tf.constant(1e-10, dtype=tf.float64)
 
-representation = tf.zeros([batch_size, 1, dag_hidden_dim])
-temp = tf.placeholder(tf.int32, [None, None, None])
-positions = tf.gather(temp, idx_var, axis=1)
-
-batch_size = tf.shape(positions)[0]
-neigh_num = tf.shape(positions)[1]
-
-idxs = tf.range(0, limit=batch_size) # [batch]
-idxs = tf.reshape(idxs, [-1, 1]) # [batch_size, 1]
-idxs = tf.tile(idxs, [1, neigh_num]) # [batch_size, neigh_num]
-indices = tf.stack((idxs,positions), axis=2) # [batch_size, neigh_num, 2]
-    
-# your code here
-print representation.get_shape()
-print indices.get_shape()
-
-result = tf.gather_nd(representation, indices)
-print result.get_shape()
+with tf.Session() as sess:
+    print (sess.run(c))
