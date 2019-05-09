c0c1f50fd72ddea9df47f2171dfa77048a9f3a2d
Joohyung Kim <crybaby26@yonsei.ac.kr>
2019-05-09 16:30:07 +0900

    debugging for nan loss issue

--- bidir_dag_lstm/G2S_model_graph.py
+++ bidir_dag_lstm/G2S_model_graph.py
@@ -121,7 +121,7 @@ class ModelGraph(object):
                     dtype=tf.float32))
 
         ## calculating loss
-        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
             logits=logits,
             labels=tf.one_hot(self.answers, options.class_num)))
  
